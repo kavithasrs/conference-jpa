c854200bd60157635de743b476aca6da839a5ed5
kavithasimac <kavithasubbiah@hotmail.com>
2021-09-18 23:08:07 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/conference/ConferenceApplication.java
@@ -0,0 +1,16 @@
+package com.pluralsight.conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ConferenceApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceApplication.class, args);
+	}
+
+}
